cde6b18b192f096e20a8dc44d949a34d6f29d90a
emmonbea <moskaleviluak@icloud.com>
2024-12-24 05:43:19 +0500

    feat(currency-wallet): add logger initialization

--- gw_currency-wallet/cmd/main.go
+++ gw_currency-wallet/cmd/main.go
@@ -4,8 +4,16 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"log/slog"
+	"os"
 
 	"github.com/emmonbear/wallet-exchanger/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -19,11 +27,33 @@ func main() {
 	cfg := config.MustLoad(*configPath)
 	fmt.Println(cfg)
 
-	// TODO: init logger: slog
+	log := setupLogger(cfg.Env)
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// TODO: init storage: postgres
 
 	// TODO: init router: chi, "chi render" (gin)
 
 	// TODO: run server:
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
