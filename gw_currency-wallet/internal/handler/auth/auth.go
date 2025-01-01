53d75a1fa7372c5ea03cfddd9631ba0f37fe8d6b
Ilya Moskalev <moskaleviluak@icloud.com>
2025-01-01 11:48:57 +0500

    feat(currency-wallet): add a user initialization handler

---
+++ gw_currency-wallet/internal/handler/auth/auth.go
@@ -0,0 +1,24 @@
+package auth
+
+import "github.com/gin-gonic/gin"
+
+type AuthHandler interface {
+	SignUp(ctx *gin.Context)
+	SignIn(ctx *gin.Context)
+}
+
+type handler struct {
+
+}
+
+func NewHandler() *handler {
+	return &handler{}
+}
+
+func (h *handler) SignUp(ctx *gin.Context) {
+
+}
+
+func (h *handler) SignIn(ctx *gin.Context) {
+
+}
