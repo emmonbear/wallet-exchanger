89342c4877a4f0da55130885047b2660cc38ce48
Ilya Moskalev <moskaleviluak@icloud.com>
2025-01-01 12:18:38 +0500

    feat(currency-wallet): add a handler for user's wallet transactions

---
+++ gw_currency-wallet/internal/handler/wallet/wallet.go
@@ -0,0 +1,23 @@
+package wallet
+
+import "github.com/gin-gonic/gin"
+
+type WalletHandler interface {
+	Deposit(ctx *gin.Context)
+	Withdraw(ctx *gin.Context)
+}
+
+type handler struct {
+}
+
+func NewHandler() *handler {
+	return &handler{}
+}
+
+func (h *handler) Deposit(ctx *gin.Context) {
+
+}
+
+func (h *handler) Withdraw(ctx *gin.Context) {
+
+}
