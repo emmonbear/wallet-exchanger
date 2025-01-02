ac4eda226881bac766e362f8230c827da43ee4eb
Ilya Moskalev <moskaleviluak@icloud.com>
2025-01-02 13:36:49 +0500

    feat(currency-wallet): add a field discriminator to link the structure to the User table database

--- gw_currency-wallet/internal/model/user.go
+++ gw_currency-wallet/internal/model/user.go
@@ -1,7 +1,7 @@
 package model
 
 type User struct {
-	Id       int
+	Id       int    `json:"-" db:"id"`
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 	Email    string `json:"email" binding:"required,email"`
