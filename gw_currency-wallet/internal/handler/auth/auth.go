c8e0cbe6f074d95741397efb35739aa996b6af93
Ilya Moskalev <moskaleviluak@icloud.com>
2025-01-02 07:22:52 +0500

    feat(currency-wallet): add a method for user registration and update the constructor

--- gw_currency-wallet/internal/handler/auth/auth.go
+++ gw_currency-wallet/internal/handler/auth/auth.go
@@ -1,6 +1,14 @@
 package auth
 
-import "github.com/gin-gonic/gin"
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/emmonbear/wallet-exchanger/internal/lib/logger/sl"
+	"github.com/emmonbear/wallet-exchanger/internal/model"
+	"github.com/emmonbear/wallet-exchanger/internal/service"
+	"github.com/gin-gonic/gin"
+)
 
 type AuthHandler interface {
 	SignUp(ctx *gin.Context)
@@ -8,14 +16,33 @@ type AuthHandler interface {
 }
 
 type handler struct {
+	logger   *slog.Logger
+	services *service.Service
 }
 
-func NewHandler() *handler {
-	return &handler{}
+func NewHandler(logger *slog.Logger, services *service.Service) *handler {
+	return &handler{
+		logger:   logger,
+		services: services,
+	}
 }
 
 func (h *handler) SignUp(ctx *gin.Context) {
+	var input model.User
+	if err := ctx.BindJSON(&input); err != nil {
+		sl.NewErrorResponse(ctx, http.StatusBadRequest, err.Error(), h.logger, err)
+		return
+	}
 
+	id, err := h.services.AuthService.CreateUser(input)
+	if err != nil {
+		sl.NewErrorResponse(ctx, http.StatusInternalServerError, err.Error(), h.logger, err)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 }
 
 func (h *handler) SignIn(ctx *gin.Context) {
