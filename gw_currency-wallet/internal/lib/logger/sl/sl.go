3255d51a78035fb7b4d2d80e70b715d481fbc306
Ilya Moskalev <moskaleviluak@icloud.com>
2025-01-02 07:21:51 +0500

    feat(currency-wallet): add handler error handling function

--- gw_currency-wallet/internal/lib/logger/sl/sl.go
+++ gw_currency-wallet/internal/lib/logger/sl/sl.go
@@ -1,10 +1,25 @@
 package sl
 
-import "log/slog"
+import (
+	"log/slog"
+
+	"github.com/gin-gonic/gin"
+)
+
+type response struct {
+	Message string `json:"message"`
+}
 
 func Err(err error) slog.Attr {
 	return slog.Attr{
 		Key:   "error",
 		Value: slog.StringValue(err.Error()),
 	}
+}
+
+func NewErrorResponse(
+	ctx *gin.Context, statusCode int, message string, log *slog.Logger, err error,
+) {
+	log.Error(message, Err(err))
+	ctx.AbortWithStatusJSON(statusCode, response{Message: message})
 }
