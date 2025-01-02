8fc847338518ed7d9abf53f2979a61754a4d889b
Ilya Moskalev <moskaleviluak@icloud.com>
2025-01-02 13:35:32 +0500

    feat(currency-wallet): add GenerateToken method for user login in the service layer

--- gw_currency-wallet/internal/service/auth/auth.go
+++ gw_currency-wallet/internal/service/auth/auth.go
@@ -3,15 +3,24 @@ package auth
 import (
 	"crypto/sha1"
 	"fmt"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/emmonbear/wallet-exchanger/internal/model"
 	"github.com/emmonbear/wallet-exchanger/internal/repository/auth"
 )
 
 const salt = "1asdqwe2@#!@sadwqe#!,;dgl"
+const tokenTTL = 12 * time.Hour
+const signingKey = "aqwec@4qvhl;#2;lg"
 
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 type AuthService interface {
 	CreateUser(user model.User) (int, error)
+	GenerateToken(username, password string) (string, error)
 }
 
 type service struct {
@@ -25,6 +34,23 @@ func NewService(repo auth.AuthRepository) *service {
 func (s *service) CreateUser(user model.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *service) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func generatePasswordHash(password string) string {
