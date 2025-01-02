21b8e2b30e0c89d0ba0e32eecf0e5d3ffb232128
Ilya Moskalev <moskaleviluak@icloud.com>
2025-01-02 07:16:44 +0500

    feat(currency-wallet): add password hashing in CreateUser service

--- gw_currency-wallet/internal/service/auth/auth.go
+++ gw_currency-wallet/internal/service/auth/auth.go
@@ -1,9 +1,35 @@
 package auth
 
-type AuthService interface{}
+import (
+	"crypto/sha1"
+	"fmt"
 
-type service struct{}
+	"github.com/emmonbear/wallet-exchanger/internal/model"
+	"github.com/emmonbear/wallet-exchanger/internal/repository/auth"
+)
 
-func NewService() *service {
-	return &service{}
+const salt = "1asdqwe2@#!@sadwqe#!,;dgl"
+
+type AuthService interface {
+	CreateUser(user model.User) (int, error)
+}
+
+type service struct {
+	repo auth.AuthRepository
+}
+
+func NewService(repo auth.AuthRepository) *service {
+	return &service{repo: repo}
+}
+
+func (s *service) CreateUser(user model.User) (int, error) {
+	user.Password = generatePasswordHash(user.Password)
+	return s.repo.CreateUser(user)
+}
+
+func generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
 }
