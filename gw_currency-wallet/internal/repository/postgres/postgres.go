c36034d803b9d00c028c3a72ac2f0250403685bb
Ilya Moskalev <moskaleviluak@icloud.com>
2025-01-03 12:35:51 +0500

    feat(repository): add Database interface and transaction method

--- gw_currency-wallet/internal/repository/postgres/postgres.go
+++ gw_currency-wallet/internal/repository/postgres/postgres.go
@@ -9,16 +9,23 @@ import (
 )
 
 const (
-	UserTable     = "users"
-	WalletTable   = "wallets"
-	CurrencyTable = "currency"
+	UsersTable        = "users"
+	UserBalancesTable = "user_balances"
+	CurrencyTable     = "currency"
 )
 
 type Storage struct {
 	db *sqlx.DB
 }
 
-func New(cfg *config.Config) (*sqlx.DB, error) {
+type Database interface {
+	Close() error
+	WithTransaction(fn TransactionFunc) error
+}
+
+type TransactionFunc func(tx *sqlx.Tx) error
+
+func New(cfg *config.Config) (*Storage, error) {
 	connStr := fmt.Sprintf(
 		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
 		cfg.StorageConfig.DBHost,
@@ -33,9 +40,29 @@ func New(cfg *config.Config) (*sqlx.DB, error) {
 		return nil, fmt.Errorf("failed to connect to database: %v", err)
 	}
 
-	return db, nil
+	return &Storage{db: db}, nil
 }
 
 func (s *Storage) Close() error {
 	return s.db.Close()
+}
+
+func (s *Storage) WithTransaction(fn TransactionFunc) error {
+	tx, err := s.db.Beginx()
+	if err != nil {
+		return fmt.Errorf("failed to begin transaction: %w", err)
+	}
+
+	if err := fn(tx); err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("transaction rollback failed: %w, original error: %w", rbErr, err)
+		}
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("failed to commit transaction: %w", err)
+	}
+
+	return nil
 }
