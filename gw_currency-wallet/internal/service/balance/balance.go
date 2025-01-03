8904da273931b6c01c6016c47667202857949b74
Ilya Moskalev <moskaleviluak@icloud.com>
2025-01-03 14:58:20 +0500

    feat(service): add GetBalance service method

--- gw_currency-wallet/internal/service/balance/balance.go
+++ gw_currency-wallet/internal/service/balance/balance.go
@@ -1,9 +1,22 @@
 package balance
 
-type BalanceService interface{}
+import (
+	"github.com/emmonbear/wallet-exchanger/internal/model"
+	"github.com/emmonbear/wallet-exchanger/internal/repository/balance"
+)
 
-type service struct{}
+type BalanceService interface {
+	GetBalance(userID int) (model.Balance, error)
+}
 
-func NewService() *service {
-	return &service{}
+type service struct {
+	repo balance.BalanceRepository
+}
+
+func NewService(repo balance.BalanceRepository) *service {
+	return &service{repo: repo}
+}
+
+func (s *service) GetBalance(userID int) (model.Balance, error) {
+	return s.repo.GetBalance(userID)
 }
