2ed2a9d6cc9cc8479d79e7c3df311bc70c397cf9
Ilya Moskalev <moskaleviluak@icloud.com>
2025-01-09 14:40:22 +0500

    refactor(logger): simplify response handling and logging functions
    - Removed unnecessary response struct and refactored error and success response functions.
    - Updated logging functions to improve consistency and readability.
    - Simplified the LogRequest function for better logging output.
    - Ensured error and success responses are handled with concise and clear methods.

--- gw_currency-wallet/internal/lib/logger/sl/sl.go
+++ gw_currency-wallet/internal/lib/logger/sl/sl.go
@@ -6,10 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type response struct {
-	Message string `json:"message"`
-}
-
 func Err(err error) slog.Attr {
 	return slog.Attr{
 		Key:   "error",
@@ -17,14 +13,36 @@ func Err(err error) slog.Attr {
 	}
 }
 
-func NewErrorResponse(
-	ctx *gin.Context, statusCode int, message string, log *slog.Logger, err error,
-) {
-	log.Error(message,
-		Err(err),
+func NewErrorResponse(ctx *gin.Context, statusCode int, message string) {
+	ctx.AbortWithStatusJSON(statusCode, gin.H{
+		"error": message})
+}
+
+func NewSuccessResponse(ctx *gin.Context, statusCode int, message gin.H) {
+	ctx.JSON(statusCode, gin.H{
+		"message": message,
+	})
+}
+
+func LogRequest(logger *slog.Logger, ctx *gin.Context, operation string) *slog.Logger {
+	return logger.With(
+		slog.String("operation", operation),
 		slog.String("method", ctx.Request.Method),
 		slog.String("path", ctx.Request.URL.Path),
-		slog.String("client_ip", ctx.ClientIP()),
+		slog.String("remote_addr", ctx.ClientIP()),
+		slog.String("user_agent", ctx.Request.UserAgent()),
 	)
-	ctx.AbortWithStatusJSON(statusCode, response{Message: message})
+}
+
+func LogError(logger *slog.Logger, operation string, err error, details ...interface{}) {
+	logger.Error("Error occurred", append([]interface{}{
+		slog.String("operation", operation),
+		slog.String("error", err.Error()),
+	}, details...)...)
+}
+
+func LogInfo(logger *slog.Logger, operation string, message string, details ...interface{}) {
+	logger.Info(message, append([]interface{}{
+		slog.String("operation", operation),
+	}, details...)...)
 }
